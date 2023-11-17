ef5e41feb4cceea3e7126fdc6fa3a60d7ab48105
NeelabhKaushik <E21CSEU0007@bennett.edu.in>
2023-11-17 10:09:59 +0530

    deplyment bugs!!

--- components/ProfilePostFeed.tsx
+++ components/ProfilePostFeed.tsx
@@ -1,20 +1,15 @@
 "use client";
 
-import { INFINITE_SCROLL_PAGINATION_RESULTS } from "@/config";
 import { ExtendedPost } from "@/types/db";
-import { useIntersection } from "@mantine/hooks";
-import { useInfiniteQuery } from "@tanstack/react-query";
-import axios from "axios";
-import { Loader2 } from "lucide-react";
-import { FC, useEffect, useRef } from "react";
-import { useSession } from "next-auth/react";
 import Post from "./Post";
+import { useSession } from "next-auth/react";
 
 interface ProfilePostFeedProps {
   initialPosts: ExtendedPost[];
 }
 
 const ProfilePostFeed = ({ initialPosts }: ProfilePostFeedProps) => {
+  const session = useSession();
   const posts = initialPosts?.flatMap((page) => page) ?? initialPosts;
 
   return (
@@ -27,7 +22,6 @@ const ProfilePostFeed = ({ initialPosts }: ProfilePostFeedProps) => {
         }, 0);
 
         const currentVote = post.votes.find(
-          //@ts-ignore
           (vote: any) => vote.userId === session?.user?.id
         );
 
