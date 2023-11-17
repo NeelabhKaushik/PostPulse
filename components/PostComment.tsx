c5f3102dc9c0ca4f68bc5fa64ee615863f0dc5b9
NeelabhKaushik <E21CSEU0007@bennett.edu.in>
2023-11-17 11:49:08 +0530

    recalling changes

--- components/PostComment.tsx
+++ components/PostComment.tsx
@@ -74,11 +74,11 @@ const PostComment = ({
           className="h-6 w-6"
         />
         <div className="ml-2 flex items-center gap-x-2">
-          <Link href={`/u/${comment.author.id}`} className="underline">
+          {/* <Link href={`/u/${comment.author.id}`} className="underline"> */}
             <p className="text-sm font-medium text-gray-900">
               u/{comment.author.name}
             </p>
-          </Link>
+          {/* </Link> */}
           <p className="max-h-40 truncate text-xs text-zinc-500">
             {formatTimeToNow(new Date(comment.createdAt))}
           </p>
